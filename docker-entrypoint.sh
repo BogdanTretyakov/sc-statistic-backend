78d9840f6b16fa24fa861fdd04f27f2c9058f968
Bogdan Tretyakov <bogdan@tretyakov.dev>
2026-02-06 03:58:13 +0300

    fix: deploy
    fix: deploy

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,11 +6,6 @@ yarn install --frozen-lockfile
 
 export NODE_ENV=production
 
-until yarn prisma db pull > /dev/null 2>&1; do
-  echo "Waiting for postgres..."
-  sleep 2
-done
-
 yarn prisma generate
 yarn build
 yarn prisma migrate deploy
